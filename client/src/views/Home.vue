36788c85f185ac3f3ab221603a0e0bed35279b5a
madriesen <martijn.driesen5@hotmail.com>
2020-10-29 18:29:11 +0100

    update button

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -8,16 +8,28 @@
       <home-selection-item link="/orders">Orders</home-selection-item>
       <home-selection-item link="/maintenance">Maintenance</home-selection-item>
     </ul>
+    <button
+      @click="shutdown"
+      class=" text-white bg-red-600 px-8 py-6 mt-16 rounded"
+    >
+      Shutdown
+    </button>
   </div>
 </template>
 
 <script>
 import HomeSelectionItem from "@/components/home/HomeSelectionItem";
+import axios from "axios";
 
 export default {
   name: "Home",
   components: {
     HomeSelectionItem
+  },
+  methods: {
+    shutdown() {
+      axios.get(process.env.VUE_APP_API_URL + "/shutdown");
+    }
   }
 };
 </script>
