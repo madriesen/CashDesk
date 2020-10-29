d9251a9efa6706f07e904680bbf3d95b6a63f015
madriesen <martijn.driesen5@hotmail.com>
2020-10-29 16:05:26 +0100

    add logo

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,9 +2,11 @@
   <div
     class="max-h-screen max-w-screen h-screen bg-white flex flex-col font-mono"
   >
-    <nav id="nav" class="bg-black text-white px-16 py-5 flex">
+    <nav id="nav" class="bg-black text-white px-16 py-5 flex items-center">
       <div class="flex-1">
-        <router-link to="/" id="logo">[logo]</router-link>
+        <router-link to="/" id="logo">
+          <img src="@/assets/logo.png" alt="Orders logo" height="50" width="100"/>
+        </router-link>
       </div>
       <clock />
     </nav>
